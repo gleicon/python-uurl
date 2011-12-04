358e7fb62f4795adcfd3373c4a113ca62f3a212f
Gleicon Moraes <gleicon@gmail.com>
2011-12-04 21:46:42 -0200

    API-fication and index fixes"

--- uurl.py
+++ uurl.py
@@ -1,4 +1,4 @@
-from bottle import route, redirect, run, debug, send_file, abort, request, ServerAdapter, template
+from bottle import route, redirect, run, debug, send_file, abort, request, ServerAdapter, template, response
 import redis
 import json
 from redis_util import _get_url_stats_by_uid, _update_encoded_url_data, _update_url_data, _get_url_by_uid
@@ -44,22 +44,27 @@ def stats(uid):
     except Exception, e:
         jc = None
     stats = _get_url_stats_by_uid(redis_cli, uid)
+    response.content_type="application/json"
     if jc != None: return "%s(%s)"% (jc, json.dumps(stats))
     return json.dumps(stats)
 
 @route('/url', method='POST')
 def post_url():
     url = request.POST['url']
-    custom_url = request.POST['custom_url']
+    try:
+        custom_url = request.POST['custom_url']
+    except Exception, e:
+        custom_url = None
     if custom_url == "": custom_url = None
     if url != None and len(url) > 1:
         if url.find(BASE_URL) > -1: abort(404, 'invalid url')
         if url.find('http') < 0: url = "http://%s" % url
         uurl = _update_url_data(redis_cli, url, custom_url)
         if uurl == None: abort(404, 'empty request')
-        return template('resp', uurl=uurl, base_url=BASE_URL)
+        response.content_type = "application/json"
+        return '{"uurl":%s, "url": "%s", "base_url": "%s"}' % (uurl, url, BASE_URL)
     else:
-        redirect('/')
+        abort(500, "empty url parameter")
 
 class GEventServerAdapter(ServerAdapter):
     def run(self, handler):
